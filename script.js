4152625c059451d4a86a9b406e0fc98040a2b3b8
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-04 14:55:18 -0400

    saving task for each hour

--- script.js
+++ script.js
@@ -8,34 +8,43 @@ $("#currentDay").text(currentDay);
 $(".saveBtn").on('click', function() {
     // get form values
     var taskHour = $(this).parent().attr("id");
+    console.log(taskHour)
     var taskText = $("textarea").val();   
-     
+    console.log(taskText)
+
     //save it to local storage
     localStorage.setItem(taskHour, taskText);
     
   });
 
 
-
-
-var currentHour = moment().hour();
-$(".hour").each(function() {
-    var timeBlock = parseInt($(this).prop("id"));
-    console.log(timeBlock)
-    if (timeBlock < currentHour && timeBlock > currentHour - 6){
-        $(this).addClass("list-group-item-danger")
-        console.log(this)
-    }
-})
-
-
-
-
+  
 var loadTasks = function()   {
     // localStorage.getItem(taskText);
     var nineAm = localStorage.getItem("9");
     $("#9 .description").val(nineAm);
-    
+
+    var twoPm = localStorage.getItem("14");
+    $("#14 .description").val(twoPm);
+    // console.log(twoPm)
 }
 
 loadTasks();
+
+
+
+// var currentHour = moment().hour();
+// $(".time-block").each(function() {
+//     var timeBlock = parseInt($(this).prop("id"));
+//     console.log(timeBlock)
+//     if (timeBlock < currentHour){
+//         $(".description").addClass('past');
+//     }
+//     else if (timeBlock == currentHour) {
+//         $(".description").removeClass('past');
+//         $(".description").addClass('present');
+//     }
+// })
+
+
+
