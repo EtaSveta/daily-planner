863a50802e976f42bba1dbe53ae9aa048dae12ca
Svetlana Nesterova <sveta59n@yahoo.com>
2022-09-04 15:34:02 -0400

    all done

--- script.js
+++ script.js
@@ -2,49 +2,69 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDay);
 
-
-
-
+// save to local sorage with SAVE button
 $(".saveBtn").on('click', function() {
     // get form values
     var taskHour = $(this).parent().attr("id");
-    console.log(taskHour)
-    var taskText = $("textarea").val();   
-    console.log(taskText)
-
+    var taskText = $(this).parent().find("textarea").val();;   
+    
     //save it to local storage
     localStorage.setItem(taskHour, taskText);
-    
   });
 
 
-  
+// get tasks from local storage  
 var loadTasks = function()   {
-    // localStorage.getItem(taskText);
     var nineAm = localStorage.getItem("9");
     $("#9 .description").val(nineAm);
 
+    var tenAm = localStorage.getItem("10");
+    $("#10 .description").val(tenAm);
+
+    var elevAm = localStorage.getItem("11");
+    $("#11 .description").val(elevAm);
+
+    var twelveAm = localStorage.getItem("12");
+    $("#12 .description").val(twelveAm);
+
+    var onePm = localStorage.getItem("13");
+    $("#13 .description").val(onePm);
+
     var twoPm = localStorage.getItem("14");
     $("#14 .description").val(twoPm);
-    // console.log(twoPm)
+
+    var threePm = localStorage.getItem("15");
+    $("#15 .description").val(threePm);
+
+    var fourPm = localStorage.getItem("16");
+    $("#16 .description").val(fourPm);
+
+    var fivePm = localStorage.getItem("17");
+    $("#17 .description").val(fivePm);
 }
 
 loadTasks();
 
 
+// current hour of the day
+var currentHour = moment().hour();
 
-// var currentHour = moment().hour();
-// $(".time-block").each(function() {
-//     var timeBlock = parseInt($(this).prop("id"));
-//     console.log(timeBlock)
-//     if (timeBlock < currentHour){
-//         $(".description").addClass('past');
-//     }
-//     else if (timeBlock == currentHour) {
-//         $(".description").removeClass('past');
-//         $(".description").addClass('present');
-//     }
-// })
+//colorcode each hour section
+$(".hour").each(function() {
+    var timeBlock = parseInt($(this).prop("id"));
+    console.log(timeBlock)
+    if (timeBlock < currentHour){
+        $(".description").addClass('past');
+    }
+    else if (timeBlock == currentHour) {
+        $(this).parent().find(".description").removeClass('past');
+        $(this).parent().find(".description").addClass('present');
+    }
+    else {
+        $(this).parent().find(".description").removeClass('present');
+        $(this).parent().find(".description").addClass('future');
+    }
+})
 
 
 
